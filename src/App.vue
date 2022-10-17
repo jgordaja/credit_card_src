1a84744725cfa7a9b1635d3d884a0bc4e47e396e
Yuliia Hordieieva <yusya.gordeeva@gmail.com>
2022-10-17 22:58:47 +0300

    added project

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="wrapper" id="app">
+		<CardForm :form-data="formData"/>
+	</div>
+</template>
+
+<script>
+import CardForm from '@/components/CardForm';
+
+export default {
+	name: 'app',
+	components: {
+		CardForm,
+	},
+	data() {
+		return {
+			formData: {
+				cardName: '',
+				cardNumber: '',
+				cardMonth: '',
+				cardYear: '',
+				cardCvv: ''
+			}
+		}
+	},
+}
+</script>
+
+<style lang="scss">
+	@import '../src/assets/style.scss';
+</style>
